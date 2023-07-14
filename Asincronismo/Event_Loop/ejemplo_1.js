9921afec5d98f4378a257ea62427cc03f66ceac3
Mauricio Fernandez <gabriel187202@live.com.mx>
2023-07-13 20:12:48 -0500

    Se crean los archivos para asincronia y demostracion de ellos clase 3 finalizada

---
+++ Asincronismo/Event_Loop/ejemplo_1.js
@@ -0,0 +1,5 @@
+//Que va primero
+console.log("1");
+console.log("2");
+console.log("3");
+
---
+++ Asincronismo/Event_Loop/ejemplo_3.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log("1");
+}, 5000);
+
+setTimeout(() => {
+    console.log("2");
+}, 2000);
+
+for(i = 0; i < 100; i++){
+    console.log(i);
+}
+
+console.log("Fin?");
